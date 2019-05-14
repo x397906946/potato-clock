8f6276d3f5a83fce7fde51269ebeb473b3e350db
Mr.X <397906946@qq.com>
2019-05-14 10:31:19 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from 'antd';
+import * as React from 'react';
+import './App.scss';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
